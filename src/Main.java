6c72ce750a87c4e53d8e29df91fbdb1a40e945bd
Folomeisdg <ywei19@coe.edu>
2023-01-12 09:49:50 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int MAX = 1000;
+        int randint = rand.nextInt(MAX);
+        System.out.println(randint);
+        System.out.println("Random value have been generated between 0 to " + MAX);
+        boolean notfound = true;
+        while (notfound) {
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Input a possible int : ");
+            int guessint = scan.nextInt();
+
+
+            if (guessint == randint) {
+                notfound = false;
+            } else if (guessint < randint) {
+                System.out.println("The input num is smaller. ");
+            } else if (guessint > randint) {
+                System.out.println("The input num is bigger. ");
+            }
+        }
+        System.out.println("Congras, you find the number "+ randint);
+    }
+
+}
+
+
